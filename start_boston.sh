9cc24e89529b0de37d4a60b1893bc9af28f5f778
normanius <normanius@gmail.com>
2022-11-18 22:57:00 +0100

    Improved output for start_boston.sh

--- start_boston.sh
+++ start_boston.sh
@@ -11,6 +11,10 @@ CF_COUNT=50
 GENERATOR_MODE=open_set
 RESULTS_DIR="./out_new"
 
+print_title () {
+    _bar=$(printf '=%.0s' {1..60})
+    printf "\n${_bar}\n${1}\n${_bar}\n\n"
+}
 
 mkdir -p "$RESULTS_DIR"
 
@@ -24,7 +28,6 @@ if [ ! -f "$RESULTS_DIR/params.json" ]; then
     exit -2
 fi
 
-
 # Command to create the params file
 # python "generativeopenset/create_experiment.py" \
 #         --result_dir "${RESULTS_DIR}" \
@@ -32,12 +35,12 @@ fi
 #         --hypothesis "boston simple" \
 #         --image_size 200
 
-
-
-# Train the intial generative model (E+G+D) and the initial classifier (C_K)
+# Train the initial generative model (E+G+D) and the initial classifier (C_K)
+print_title "TRAIN GAN"
 python src/train_gan.py --epochs $GAN_EPOCHS --result_dir "$RESULTS_DIR"
 
 # Baseline: Evaluate the standard classifier (C_k+1)
+print_title "EVALUATE C_k+1 CLASSIFIER"
 python src/evaluate_classifier.py --result_dir "$RESULTS_DIR" --mode baseline
 python src/evaluate_classifier.py --result_dir "$RESULTS_DIR" --mode weibull
 
@@ -45,17 +48,21 @@ cp $RESULTS_DIR/checkpoints/classifier_k_epoch_00${GAN_EPOCHS}.pth \
    $RESULTS_DIR/checkpoints/classifier_kplusone_epoch_00${GAN_EPOCHS}.pth
 
 # Generate a number of counterfactual images (in the K+2 by K+2 square grid format)
+print_title "GENERATE COUNTERFACTUAL IMAGES"
 python src/generate_${GENERATOR_MODE}.py \
     --result_dir $RESULTS_DIR \
     --count $CF_COUNT
 
 # Automatically label the rightmost column in each grid (ignore the others)
+print_title "CREATE LABELS"
 python src/auto_label.py --output_filename "$RESULTS_DIR/generated_images_${GENERATOR_MODE}.dataset"
 
 # Train a new classifier, now using the aux_dataset containing the counterfactuals
+print_title "TRAIN C_k+1 CLASSIFIER"
 python src/train_classifier.py --epochs $CLASSIFIER_EPOCHS --aux_dataset "$RESULTS_DIR/generated_images_${GENERATOR_MODE}.dataset"
 
 # Evaluate the C_K+1 classifier, trained with the augmented data
+print_title "EVALUATE C_k+1 CLASSIFIER"
 python src/evaluate_classifier.py --result_dir "$RESULTS_DIR" --mode fuxin
 
 ./print_results.sh
